bf3935b06be8e52b1a077881ac897485121cf64c
rdwebushi <rushikesh26deshpande@gmail.com>
2023-08-19 19:09:13 +0530

    day_1 in nodejs

---
+++ day1/code1.js
@@ -0,0 +1,19 @@
+//addition of two numbers
+let a = 10;
+let b = 20;
+let c = a + b;
+
+console.log("value of c ",c);
+
+// number entered by user
+let sqrt_a = Math.sqrt(35);
+console.log("sqrt of a  is",sqrt_a);
+console.log(`area of triangle  ${sqrt_a}`);
+// calculate area of traingle
+
+let height = 20;
+let base = 30;
+
+let area = (base * height)/2;
+// console.log("area of triangle ", area);
+console.log(`area of triangle  ${area}`);
